a5337d6b4306f94db4f23c1f871a49413d772290
spaul13 <sibendu.paul01@gmail.com>
2019-04-13 01:07:53 -0400

    3-sim-req

--- src/TCPServer_Ondemand_Main.java
+++ src/TCPServer_Ondemand_Main.java
@@ -32,63 +32,88 @@ public class TCPServer_Ondemand_Main {
 					
 						byte[] buf = new byte[1500];
 						byte[] videoBuf = new byte[409600];
-						int fid = 0;
+						//int fid = 0;
+						//int fid1 = 0;
+						//int fid2 = 0;
+						int[] fid = new int[] {0, 0, 0};
+						
 						Long start_input = System.currentTimeMillis();
 						is.read(buf);
 						System.out.println("\n Received Buffer is = " + buf.toString());
 						System.out.println("\n Length of the input buffer received is = " + (buf.toString().length()-1));
 						
 						//resolving the input stream to get the fid
-						for (int i = 0; i < buf.toString().length() - 1; i++) fid = fid * 10 + (buf[i] - '0');
-						System.out.println("Actual fid: " + Integer.toString(fid));
-						String videoFile = videoPath + Integer.toString(fid) + ".mp4";
-						System.out.println("Video File name To send: " + videoFile);
-						Long start_rs = System.currentTimeMillis();
-					
-						int account = 0;
-						File video = new File(videoFile);
-						System.out.println("\n the videofile size is = " + video.length());
-						FileInputStream fis = new FileInputStream(video);
-						int sendBufSize = socket.getSendBufferSize();
-						System.out.println("\n Send Buffer size = " + Integer.toString(sendBufSize));
-						Long start_send = System.currentTimeMillis();
-						
-						
-						account = fis.read(videoBuf);
-						System.out.println("\n the total size is = " + account);
-						
-						buf[0] = 'S';
-                        buf[1] = 'J';
-                        temp = fid;
-                        for (int i = 7; i > 1; i--) {
-                            buf[i] = (byte) (char) ((temp % 10) + '0');
-                            temp /= 10;
-                        }
-                        temp = account;
-                        for (int i = 13; i > 7; i--) {
-                            buf[i] =  (byte) (char) ((temp % 10) + '0');
-                            temp /= 10;
-                        }
-                        for (int i = 19; i > 14; i--) {
-                            buf[i] = 0;
-                        }
-                    
-                        os.write(buf, 0, 20);
-						os.write(videoBuf, 0, account);
-						
-						/*while ((account = fis.read(videoBuf)) != -1) 
+						//for (int i = 0; i < buf.toString().length() - 1; i++) fid = fid * 10 + (buf[i] - '0');
+						//Sending three requesting at time
+						int curr = 0;
+						for (int i = 0; i < 7; i++)fid[curr] = fid[curr] * 10 + (buf[i] - '0');
+						curr++;
+						for (int i = 7; i < 13; i++)fid[curr] = fid[curr] * 10 + (buf[i] - '0');
+						curr++;
+						for (int i = 13; i < 19; i++)fid[curr] = fid[curr] * 10 + (buf[i] - '0');
+						for (int i=0; i<3; i++)
 						{
-							System.out.println("\n the current buffer size is = " + account);
-							socket.getOutputStream().write(videoBuf, 0, account);
+							System.out.println("curr fid: " + fid[i]);
 						}
-						*/
 						
-						System.out.println("fid: " + Integer.toString(fid) + "; Send: " + Long.toString(System.currentTimeMillis() - start_send));
-						System.out.println("fid: " + Integer.toString(fid) + "; Read File + Send: " + Long.toString(System.currentTimeMillis() - start_rs));
-						System.out.println("fid: " + Integer.toString(fid) + "; Input Proc + Read File + Send: " + Long.toString(System.currentTimeMillis() - start_input));
-						os.flush();
-						//socket.getOutputStream().close();
-						System.out.println("Video transmission finished.");
+						
+						for(int j = 0; j<3 && fid[j] != 0; j++)
+						{
+							System.out.println("curr fid: " + Integer.toString(fid[j]));
+							String videoFile = videoPath + Integer.toString(fid[j]) + ".mp4";
+							System.out.println("Video File name To send: " + videoFile);
+							Long start_rs = System.currentTimeMillis();
+					
+							int account = 0;
+							File video = new File(videoFile);
+							System.out.println("\n the videofile size is = " + video.length());
+							FileInputStream fis = new FileInputStream(video);
+							int sendBufSize = socket.getSendBufferSize();
+							System.out.println("\n Send Buffer size = " + Integer.toString(sendBufSize));
+							Long start_send = System.currentTimeMillis();
+						
+						
+							account = fis.read(videoBuf);
+							System.out.println("\n the total size is = " + account);
+							
+						
+							buf[0] = 'S';
+							buf[1] = 'J';
+							temp = fid[j];
+							for (int i = 7; i > 1; i--) 
+							{
+								buf[i] = (byte) (char) ((temp % 10) + '0');
+								temp /= 10;
+							}
+							temp = account;
+							for (int i = 13; i > 7; i--) 
+							{
+								buf[i] =  (byte) (char) ((temp % 10) + '0');
+								temp /= 10;
+							}
+							for (int i = 19; i > 14; i--) 
+							{
+								buf[i] = 0;
+							}
+                    
+							os.write(buf, 0, 20);
+							
+							os.write(videoBuf, 0, account);
+						
+							/*while ((account = fis.read(videoBuf)) != -1) 
+							{
+								System.out.println("\n the current buffer size is = " + account);
+								socket.getOutputStream().write(videoBuf, 0, account);
+							}
+							 */
+						
+							System.out.println("fid: " + Integer.toString(fid[j]) + "; Send: " + Long.toString(System.currentTimeMillis() - start_send));
+							System.out.println("fid: " + Integer.toString(fid[j]) + "; Read File + Send: " + Long.toString(System.currentTimeMillis() - start_rs));
+							System.out.println("fid: " + Integer.toString(fid[j]) + "; Input Proc + Read File + Send: " + Long.toString(System.currentTimeMillis() - start_input));
+							os.flush();
+							//socket.getOutputStream().close();
+							System.out.println("Video transmission finished.");
+						}
 					}
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
